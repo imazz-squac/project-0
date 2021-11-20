e291e728fe17b06454f13ed87f44e3f9fd3391f5
mfoydl <milesfoydl@gmail.com>
2021-11-20 12:59:55 -0600

    created main component for app

--- bean-juice/src/client/index.js
+++ bean-juice/src/client/index.js
@@ -1,7 +1,7 @@
 
 import { createElement } from 'lwc';
-import MyApp from 'my/app';
+import Container from 'c/container';
 
-const app = createElement('my-app', { is: MyApp });
+const app = createElement('c-container', { is: Container });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
