00695bf8c4cc2c599aa768d3e3add30d902a5bf7
mfoydl <milesfoydl@gmail.com>
2021-11-23 15:47:04 -0600

    readded old content to new project tree

---
+++ src/client/modules/c/navItem/navItem.js
@@ -0,0 +1,6 @@
+import { LightningElement, api } from 'lwc';
+
+export default class Navbar extends LightningElement {
+    @api
+    active;
+}
---
+++ src/client/modules/c/seasonalItem/seasonalItem.js
@@ -0,0 +1,12 @@
+import { LightningElement, api } from "lwc";
+
+export default class SeasonalItem extends LightningElement {
+    @api
+    imagePath;
+
+    @api
+    imageAlt;
+
+    @api
+    itemDescription;
+}
