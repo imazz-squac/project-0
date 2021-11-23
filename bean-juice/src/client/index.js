746761c33314e0416f1befcec8edeb685524e2da
mfoydl <31784414+mfoydl@users.noreply.github.com>
2021-11-23 15:14:18 -0600

    Revert "Miles working"

--- bean-juice/src/client/index.js
+++ bean-juice/src/client/index.js
@@ -1,7 +1,7 @@
-import '@lwc/synthetic-shadow';
-import { createElement } from 'lwc';
-import Container from 'c/container';
 
-const app = createElement('c-container', { is: Container });
+import { createElement } from 'lwc';
+import MyApp from 'my/app';
+
+const app = createElement('my-app', { is: MyApp });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
