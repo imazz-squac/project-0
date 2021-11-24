6278fd972857fa7dc3a486df1be69026dffe2452
Walter <eschenbach.walter@gmail.com>
2021-11-24 16:32:38 -0600

    fix bug in products array

--- src/client/modules/my/featured/featured.js
+++ src/client/modules/my/featured/featured.js
@@ -1,3 +1,29 @@
 import { LightningElement } from 'lwc';
 
-export default class Featured extends LightningElement {}
+export default class Featured extends LightningElement {
+    productArray = [
+        {
+            imgSource: "../../../resources/featured_products/pharaoh.jpg",
+            make: "NERF ULTRA",
+            model: "PHARAOH"
+        }, {
+            imgSource: "../../../resources/featured_products/basr.jpeg",
+            make: "NERF FORTNITE",
+            model: "BASR-L"
+        }, {
+            imgSource: "../../../resources/featured_products/motostryke.jpg",
+            make: "NERF MEGA",
+            model: "MOTOSTRYKE"
+        }, {
+            imgSource: "../../../resources/featured_products/Commander.jpg",
+            make: "NERF ELITE 2.0",
+            model: "COMMANDER"
+        }, {
+            imgSource: "../../../resources/featured_products/elite.jpg",
+            make: "NERF ELITE",
+            model: "ELITE 2.0 TURBINE"
+        }
+    ]
+
+
+}
