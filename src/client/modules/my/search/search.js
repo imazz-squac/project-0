156393c95358f4bcfccb218a14664d3f37553513
Walter <eschenbach.walter@gmail.com>
2021-11-24 18:57:54 -0600

    implement @track on search component to take user input

--- src/client/modules/my/search/search.js
+++ src/client/modules/my/search/search.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Search extends LightningElement {}
+export default class Search extends LightningElement {
+    @track searchInput = "";
+
+    handleChange(event) {
+        this.searchInput = event.target.value;
+        console.log('searchInput:', this.searchInput)
+    }
+    handleSubmit(event) {
+        window.alert(this.searchInput)
+        event.preventDefault()
+    }
+}
