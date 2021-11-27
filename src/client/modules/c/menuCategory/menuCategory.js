5b978c59c0c757efcec774b6b19aff9c15f9011a
mfoydl <milesfoydl@gmail.com>
2021-11-27 15:22:42 -0600

    Completed menu page, stylings and filter functionality enhancements

--- src/client/modules/c/menuCategory/menuCategory.js
+++ src/client/modules/c/menuCategory/menuCategory.js
@@ -7,12 +7,21 @@ export default class MenuCategory extends LightningElement {
     @api
     items;
 
-    @api filterUpdate(filter){
-       let menuItems = this.template.querySelectorAll('c-menu-item');
+    @api
+    display;
 
-       menuItems.forEach(function(item){
-           item.filterChange(filter);
-       });
+    constructor(){
+        super();
+        this.display = true;
     }
 
+    @api filterUpdate(filter){
+        let menuItems = this.template.querySelectorAll('c-menu-item');
+        let hasItemDisplayed = false;
+        menuItems.forEach(function(item){ 
+            item.filterChange(filter);
+            if(item.display) hasItemDisplayed = true;
+        });
+        this.display = hasItemDisplayed;
+    }
 }
--- src/client/modules/c/menuItem/menuItem.js
+++ src/client/modules/c/menuItem/menuItem.js
@@ -21,6 +21,14 @@ export default class MenuItem extends LightningElement {
         this.display=true;
     }
 
+    renderedCallback(){
+        let imageContainer = this.template.querySelector('.item-image');
+        if(imageContainer){
+            imageContainer.style.backgroundImage = `url(${this.itemImage})`;
+        }
+        
+    }
+
     @api filterChange(filter){
         this.display = (this.tags.indexOf(filter)!=-1) ? !this.display:this.display;
     }
