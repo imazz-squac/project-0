561e56339c2f637f61371427a79079fbf0e7baf5
unknown <ryanmacmcdowell@gmail.com>
2021-11-28 23:39:41 -0500

    finalCommit

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
 export default class App extends LightningElement {}
+
+function myFunction() {
+    var x = document.getElementById("myBtn");
+    x.disabled = true;
+  }
